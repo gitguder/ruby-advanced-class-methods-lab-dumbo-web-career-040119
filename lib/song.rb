feb6661621bde59624a018ee1f8d7ebfe782a59f
Codey Pinero <codey.pinero@gmail.com>
2019-04-08 04:02:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,19 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
+  def self.create(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+    @@all << self
+  end
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-
+ 
 end
